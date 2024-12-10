0fbbab43f39a1aca4a67b4de63f4a6bd133372de
vdbergen <jef53@telenet.be>
2024-12-10 22:04:09 +0100

    day2

---
+++ day2-1.py
@@ -0,0 +1,25 @@
+f = open("day2.txt", "r")
+print(f.readline())
+
+safeCount = 0
+safe = True
+
+with open('day2.txt', 'r') as file:
+  for line in file:
+    lineList = line.strip().split()
+    lineListInt = [int(item)for item in lineList]
+    for i in range(len(lineList)-1):
+      if abs(lineListInt[i] - lineListInt[i+1]) >2:
+        safe = False
+        print(safe)
+        break
+    if safe:
+      safeCount +=1
+      print(safe)
+    else:
+      safe == True
+      print(lineList)
+print(safeCount)
+
+
+
